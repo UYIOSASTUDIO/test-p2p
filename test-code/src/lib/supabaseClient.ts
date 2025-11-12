728e84e145c0717d2f4add50fa7a5b011fae0423
Elias Schmolke <elias.schmolke@icloud.com>
2025-11-12 13:53:55 +0100

    user profile 3

--- test-code/src/lib/supabaseClient.ts
+++ test-code/src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 // src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+// Präfix zu VITE_PUBLIC_ geändert, um Vercel/Vite zu beruhigen
+import { VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY)
